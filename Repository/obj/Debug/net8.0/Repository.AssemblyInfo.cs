1b41d9b893261df6a4833a647b233aa9997255a7
MOI <hungnvse150584@fpt.edu.vn>
2025-03-24 00:01:20 +0700

    GetAllHomeStayRentalShowPricing

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5c25b6153798292f00e871c88e802fccc9440e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3114621d67418b6b87f939f63aed13a3df391a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
