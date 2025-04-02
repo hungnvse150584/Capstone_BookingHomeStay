8237f1ee01e04ad79a4e5c348af5fe8270108a67
YaosuVK <vukhanh221212@gmail.com>
2025-04-03 00:23:14 +0700

    Fix Db

---
+++ DataAccessObject/Migrations/20250402172155_FixDb20.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessObject.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDb20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "receiverID",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "senderName",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "receiverID",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "senderName",
+                table: "Messages");
+        }
+    }
+}
--- DataAccessObject/obj/Debug/net8.0/DataAccessObject.AssemblyInfo.cs
+++ DataAccessObject/obj/Debug/net8.0/DataAccessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a680404ea069b729a593f79a65ec15aa4b1893f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3348de6c8581fa06824111e95649729626678cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
