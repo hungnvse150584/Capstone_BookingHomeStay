bc03ebfe105a13fb3cd9da8b9aae06ebabc9bcd1
MOI <hungnvse150584@fpt.edu.vn>
2025-04-05 14:14:44 +0700

    Add Long, Lat for UpdateHomeStay

--- DataAccessObject/HomeStayDAO.cs
+++ DataAccessObject/HomeStayDAO.cs
@@ -1,146 +1,164 @@
-﻿using BusinessObject.Model;
-using BusinessObject.PaginatedLists;
-using DataAccessObject.BaseDAO;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using BusinessObject.Model;
+    using BusinessObject.PaginatedLists;
+    using DataAccessObject.BaseDAO;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.IdentityModel.Tokens;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace DataAccessObject
-{
-    public class HomeStayDAO : BaseDAO<HomeStay>
+    namespace DataAccessObject
     {
-        private readonly GreenRoamContext _context;
-        private const double EarthRadiusKm = 6371; // Bán kính trái đất (km)
-        public HomeStayDAO(GreenRoamContext context) : base(context)
+        public class HomeStayDAO : BaseDAO<HomeStay>
         {
-            _context = context;
-        }
-
-        public async Task<HomeStay?> ChangeHomeStayStatus(int homestayId, HomeStayStatus status, int? commissionRateId = null)
-        {
-            var homestay = await _context.HomeStays.FindAsync(homestayId);
-            if (homestay != null)
+            private readonly GreenRoamContext _context;
+            private const double EarthRadiusKm = 6371; // Bán kính trái đất (km)
+            public HomeStayDAO(GreenRoamContext context) : base(context)
             {
-                homestay.Status = status;
+                _context = context;
+            }
+
+            public async Task<HomeStay?> ChangeHomeStayStatus(int homestayId, HomeStayStatus status, int? commissionRateId = null)
+            {
+                var homestay = await _context.HomeStays.FindAsync(homestayId);
+                if (homestay != null)
+                {
+                    homestay.Status = status;
+                    await _context.SaveChangesAsync();
+                }
+
+                return await _context.HomeStays.FindAsync(homestayId);
+            }
+
+            public async Task<IEnumerable<HomeStay>> GetAllRegisterHomeStayAsync()
+            {
+                //return await _context.HomeStays
+                //            .Include(c => c.Account)
+
+                //            .ToListAsync();
+                return await _context.HomeStays
+                   .Include(h => h.Account)
+                   .Include(h => h.Reports)
+                   .Include(h => h.HomeStayRentals)
+                   .Include(h => h.ImageHomeStays)
+                   .Include(h => h.Bookings)
+                   .Include(h => h.CultureExperiences)
+                   .Include(h => h.Services)
+                   .Include(h => h.Ratings)
+                   .ToListAsync();
+            }
+
+            public async Task<HomeStay> GetHomeStayDetailByIdAsync(int id)
+            {
+                if (id <= 0)
+                {
+                    throw new ArgumentNullException($"id {id} not found");
+                }
+                var entity = await _context.Set<HomeStay>()
+                            .Include(c => c.Account)
+                            .Include(h => h.Reports)
+                             .Include(h => h.ImageHomeStays)
+                              .Include(h => h.CultureExperiences)
+                              .Include(h => h.Services)
+                              
+                   .Include(h => h.Ratings)
+                   .SingleOrDefaultAsync(c => c.HomeStayID == id);
+                if (entity == null)
+                {
+                    throw new ArgumentNullException($"Entity with id {id} not found");
+                }
+                return entity;
+            }
+
+            public async Task<IEnumerable<HomeStay>> GetAllHomeStayAsync()
+            {
+                return await _context.HomeStays
+                            .Include(h => h.Account)
+                            .Include(h => h.Reports)
+                            .Include(h => h.HomeStayRentals)
+                            .Include(h => h.ImageHomeStays)
+                            .Include(h => h.Bookings)
+                            .Include(h => h.CultureExperiences)
+                            .Include(h => h.Services)
+                            .Include(h => h.Ratings)
+                            .ToListAsync();
+            }
+
+            public async Task SaveChangesAsync()
+            {
                 await _context.SaveChangesAsync();
             }
-
-            return await _context.HomeStays.FindAsync(homestayId);
-        }
-
-        public async Task<IEnumerable<HomeStay>> GetAllRegisterHomeStayAsync()
-        {
-            //return await _context.HomeStays
-            //            .Include(c => c.Account)
-
-            //            .ToListAsync();
-            return await _context.HomeStays
-               .Include(h => h.Account)
-               .Include(h => h.Reports)
-               .Include(h => h.HomeStayRentals)
-               .Include(h => h.ImageHomeStays)
-               .Include(h => h.Bookings)
-               .Include(h => h.CultureExperiences)
-               .Include(h => h.Services)
-               .Include(h => h.Ratings)
-               .ToListAsync();
-        }
-
-        public async Task<HomeStay> GetHomeStayDetailByIdAsync(int id)
-        {
-            if (id <= 0)
+            public async Task<HomeStay> GetHomeStayByIdAsync(int id)
             {
-                throw new ArgumentNullException($"id {id} not found");
+                if (id <= 0)
+                {
+                    throw new ArgumentNullException($"id {id} not found");
+                }
+                var entity = await _context.Set<HomeStay>()
+                            .Include(c => c.Account)
+                            .Include(c => c.Services)
+                   .SingleOrDefaultAsync(c => c.HomeStayID == id);
+                if (entity == null)
+                {
+                    throw new ArgumentNullException($"Entity with id {id} not found");
+                }
+                return entity;
             }
-            var entity = await _context.Set<HomeStay>()
-                        .Include(c => c.Account)
-                        .Include(h => h.Reports)
-                         .Include(h => h.ImageHomeStays)
-                          .Include(h => h.CultureExperiences)
-                          .Include(h => h.Services)
-               .Include(h => h.Ratings)
-               .SingleOrDefaultAsync(c => c.HomeStayID == id);
-            if (entity == null)
+            public async Task<IEnumerable<HomeStay>> GetAllWithDetailsAsync()
             {
-                throw new ArgumentNullException($"Entity with id {id} not found");
-            }
-            return entity;
-        }
-
-        public async Task<IEnumerable<HomeStay>> GetAllHomeStayAsync()
-        {
-            return await _context.HomeStays
-                        .Include(h => h.Account)
-                        .Include(h => h.Reports)
-                        .Include(h => h.HomeStayRentals)
-                        .Include(h => h.ImageHomeStays)
-                        .Include(h => h.Bookings)
-                        .Include(h => h.CultureExperiences)
-                        .Include(h => h.Services)
-                        .Include(h => h.Ratings)
-                        .ToListAsync();
-        }
-
-        public async Task SaveChangesAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-        public async Task<HomeStay> GetHomeStayByIdAsync(int id)
-        {
-            if (id <= 0)
-            {
-                throw new ArgumentNullException($"id {id} not found");
-            }
-            var entity = await _context.Set<HomeStay>()
-                        .Include(c => c.Account)
-                        .Include(c => c.Services)
-               .SingleOrDefaultAsync(c => c.HomeStayID == id);
-            if (entity == null)
-            {
-                throw new ArgumentNullException($"Entity with id {id} not found");
-            }
-            return entity;
-        }
-        public async Task<IEnumerable<HomeStay>> GetAllWithDetailsAsync()
-        {
-            return await _context.HomeStays
-                .Include(h => h.HomeStayRentals)
-                .Include(h => h.Bookings)
-                 .ThenInclude(b => b.BookingDetails)
-                .Include(h => h.ImageHomeStays)
+                return await _context.HomeStays
+                    .Include(h => h.HomeStayRentals)
+                    .Include(h => h.Bookings)
+                     .ThenInclude(b => b.BookingDetails)
+                    .Include(h => h.ImageHomeStays)
                    
-                .ToListAsync();
-        }
-        public async Task<HomeStay> GetOwnerHomeStayByIdAsync(string accountId)
-        {
-            if (string.IsNullOrEmpty(accountId))
-            {
-                throw new ArgumentNullException($"id {accountId} not found");
+                    .ToListAsync();
             }
-            var entity = await _context.Set<HomeStay>()
-                        .Include(c => c.Account)
-               .FirstOrDefaultAsync(c => c.AccountID == accountId);
-            if (entity == null)
+            public async Task<HomeStay> GetOwnerHomeStayByIdAsync(string accountId)
             {
-                throw new ArgumentNullException($"Entity with id {accountId} not found");
+                if (string.IsNullOrEmpty(accountId))
+                {
+                    throw new ArgumentNullException($"id {accountId} not found");
+                }
+                var entity = await _context.Set<HomeStay>()
+                            .Include(c => c.Account)
+                   .FirstOrDefaultAsync(c => c.AccountID == accountId);
+                if (entity == null)
+                {
+                    throw new ArgumentNullException($"Entity with id {accountId} not found");
+                }
+                return entity;
             }
-            return entity;
-        }
 
-        /*public async Task<IEnumerable<HomeStay>> SearchHomeStay(string? search)
-        {
-
-        }*/
-
-        public async Task<IEnumerable<HomeStay>> GetNearestHomeStaysAsync(double userLat, double userLon, int pageIndex = 1, int pageSize = 5)
-        {
-            /*return await Task.Run(() =>
+            /*public async Task<IEnumerable<HomeStay>> SearchHomeStay(string? search)
             {
-                return _context.HomeStays
+
+            }*/
+
+            public async Task<IEnumerable<HomeStay>> GetNearestHomeStaysAsync(double userLat, double userLon, int pageIndex = 1, int pageSize = 5)
+            {
+                /*return await Task.Run(() =>
+                {
+                    return _context.HomeStays
+                        .AsEnumerable() // Chuyển sang xử lý trên RAM
+                        .Select(hs => new
+                        {
+                            HomeStay = hs,
+                            Distance = CalculateHaversineDistance(userLat, userLon, hs.Latitude, hs.Longitude)
+                        })
+                        .OrderBy(hs => hs.Distance)
+                        .Take(topN)
+                        .Select(hs => hs.HomeStay);
+                });*/
+
+                IQueryable<HomeStay> query = _context.HomeStays
+                                            .Include(hs => hs.Account)
+                                            .Include(hs => hs.ImageHomeStays)
+                                            .Include(hs => hs.Ratings);
+
+                var homeStaysWithDistance = query
                     .AsEnumerable() // Chuyển sang xử lý trên RAM
                     .Select(hs => new
                     {
@@ -148,46 +166,29 @@ namespace DataAccessObject
                         Distance = CalculateHaversineDistance(userLat, userLon, hs.Latitude, hs.Longitude)
                     })
                     .OrderBy(hs => hs.Distance)
-                    .Take(topN)
-                    .Select(hs => hs.HomeStay);
-            });*/
+                    .Select(hs => hs.HomeStay)
+                    .AsQueryable(); // Chuyển về IQueryable để hỗ trợ phân trang
 
-            IQueryable<HomeStay> query = _context.HomeStays
-                                        .Include(hs => hs.Account)
-                                        .Include(hs => hs.ImageHomeStays)
-                                        .Include(hs => hs.Ratings);
+                return PaginatedList<HomeStay>.Create(homeStaysWithDistance, pageIndex, pageSize);
+            }
 
-            var homeStaysWithDistance = query
-                .AsEnumerable() // Chuyển sang xử lý trên RAM
-                .Select(hs => new
-                {
-                    HomeStay = hs,
-                    Distance = CalculateHaversineDistance(userLat, userLon, hs.Latitude, hs.Longitude)
-                })
-                .OrderBy(hs => hs.Distance)
-                .Select(hs => hs.HomeStay)
-                .AsQueryable(); // Chuyển về IQueryable để hỗ trợ phân trang
+            public double CalculateHaversineDistance(double lat1, double lon1, double lat2, double lon2)
+            {
+                double dLat = DegreesToRadians(lat2 - lat1);
+                double dLon = DegreesToRadians(lon2 - lon1);
 
-            return PaginatedList<HomeStay>.Create(homeStaysWithDistance, pageIndex, pageSize);
-        }
+                double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                           Math.Cos(DegreesToRadians(lat1)) * Math.Cos(DegreesToRadians(lat2)) *
+                           Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
 
-        public double CalculateHaversineDistance(double lat1, double lon1, double lat2, double lon2)
-        {
-            double dLat = DegreesToRadians(lat2 - lat1);
-            double dLon = DegreesToRadians(lon2 - lon1);
+                double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
 
-            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
-                       Math.Cos(DegreesToRadians(lat1)) * Math.Cos(DegreesToRadians(lat2)) *
-                       Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+                return EarthRadiusKm * c; // Khoảng cách tính bằng km
+            }
 
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-
-            return EarthRadiusKm * c; // Khoảng cách tính bằng km
-        }
-
-        private double DegreesToRadians(double degrees)
-        {
-            return degrees * (Math.PI / 180);
+            private double DegreesToRadians(double degrees)
+            {
+                return degrees * (Math.PI / 180);
+            }
         }
     }
-}
