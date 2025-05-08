1ba764ce28c8ed08e62b50a7f235368f96649e83
YaosuVK <vukhanh221212@gmail.com>
2025-05-08 22:54:29 +0700

    Service-RoomType Stats

--- DataAccessObject/BookingDetailDAO.cs
+++ DataAccessObject/BookingDetailDAO.cs
@@ -23,5 +23,25 @@ namespace DataAccessObject
                                  .Where(d => d.BookingID == bookingId && !updatedDetailIds.Contains(d.BookingDetailID))
                                  .ToListAsync();
         }
+
+        public async Task<List<(string roomTypeName, int count)>> GetRoomTypeUsageStatsAsync(int homestayId)
+        {
+            var stats = await _context.BookingDetails
+                .Where(b => (b.Booking.Status == BookingStatus.Confirmed ||
+                       b.Booking.Status == BookingStatus.Completed ||
+                       b.Booking.Status == BookingStatus.InProgress) &&
+                       b.Booking.HomeStayID == homestayId &&
+                       b.RoomID != null)
+                .Include(b => b.Rooms)
+                    .ThenInclude(r => r.RoomTypes)
+                .GroupBy(b => b.Rooms.RoomTypes.Name)
+                .Select(g => new
+                {
+                    roomTypeName = g.Key,
+                    count = g.Count()
+                }).ToListAsync();
+
+            return stats.Select(s => (s.roomTypeName, s.count)).ToList();
+        }
     }
 }
---
+++ Service/RequestAndResponse/Response/Services/GetServiceStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.RequestAndResponse.Response.Services
+{
+    public class GetServiceStats
+    {
+        public string ServiceName { get; set; }
+        public int count { get; set; }
+    }
+}
